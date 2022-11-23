94f9479bd3a8194d36297624d8db5af83c53c79b
VlRsMD <90247966+VlRsMD@users.noreply.github.com>
2022-11-23 10:02:29 +0200

    Update

--- sent_order.py
+++ sent_order.py
@@ -1,6 +1,6 @@
 class orderOut:
-    def __init__(self, id, client_id, priority, items, max_wait, generation_time):
-        self.id = id
+    def __init__(self, order_id, client_id, priority, items, max_wait, generation_time):
+        self.order_id = order_id
         self.client_id = client_id
         self.priority = priority
         self.items = items
